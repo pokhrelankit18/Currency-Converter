ed366cda77107d108f3852960d0ea0a98c128ee2
pokhrelworks <work.ankitpokhrel@gmail.com>
2024-06-13 00:09:23 +0545

    addednewapi

--- main.js
+++ main.js
@@ -158,51 +158,47 @@ const countryList = {
     ZAR: "ZA",
     ZMK: "ZM",
     ZWD: "ZW",
-  };
+};
 
-const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/"
+const BASE_URL = "https://v6.exchangerate-api.com/v6/b2296cfbeb55459b94a6ddd8/latest/USD";
 const dropdowns = document.querySelectorAll(".dropdown select");
 const btn = document.querySelector("form button");
-const fromCurr = document.querySelector(".from select")
-const toCurr = document.querySelector(".to select")
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg");
- for(let select of dropdowns)
- {  
-for (currCode in countryList)
-{
-    let newOption = document.createElement("option")
-    newOption.innerText= currCode;
-    newOption.value = currCode;
-    select.append(newOption);
-}
-select.addEventListener("change",(evt) => {
-    updateFlag(evt.target);
-});
- }
 
- const updateFlag = (element) => {
+for (let select of dropdowns) {
+    for (let currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
     let currCode = element.value;
     let countryCode = countryList[currCode];
     let newSrc = `https://flagsapi.com/${countryCode}/shiny/64.png`;
     let img = element.parentElement.querySelector("img");
-    img.src=newSrc;
- }
+    img.src = newSrc;
+}
 
- btn.addEventListener("click", async (evt) => {
+btn.addEventListener("click", async (evt) => {
     evt.preventDefault();
     let amount = document.querySelector(".amount input");
     let amtval = amount.value;
-    if(amtval==="" || amtval<1)
-    {
-        amtval=1;
-        amount.value="1";
+    if (amtval === "" || amtval < 1) {
+        amtval = 1;
+        amount.value = "1";
     }
-    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`
-    let response = await fetch(URL);
+    let response = await fetch(BASE_URL);
     let data = await response.json();
-    let rate = data[toCurr.value.toLowerCase()];
+    let rate = data.conversion_rates[toCurr.value];
     
     let finalAmount = amtval * rate;
-    msg.innerText = `${amtval} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
- });
-
+    msg.innerText = `${amtval} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+});
